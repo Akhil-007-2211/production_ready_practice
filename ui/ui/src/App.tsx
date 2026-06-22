51d8ba158dfde9eae94ff7ac6970f8b635df75a1
Akhil-007-2211 <akhil.gunthati@gmail.com>
2026-06-22 23:44:18 +0530

    Added Signup Component

--- ui/ui/src/App.tsx
+++ ui/ui/src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
 import { OrbitProgress } from 'react-loading-indicators'
+import Signup from './Signnup'
+import { useState } from 'react';
 
 function App() {
-
+  let [showSignup, setShowSignup] = useState(false);
   return (
     <>
       <div className="App">
+        <button className='signup-btn'
+          onClick={() => {setShowSignup(true)}}>
+          Signup
+        </button>
+        {(showSignup ? <Signup /> : (<>
         <OrbitProgress variant="spokes" color="#32cd32" size="medium" text="" textColor="" />
         <h1>App building is in progress</h1>
+        </>))}
       </div>
     </>
   )
